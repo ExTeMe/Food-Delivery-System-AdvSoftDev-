03d3a16f6bf11fe8647b7a3916ed9fac24ef30f0
BenzChua31 <benzchua31@gmail.com>
2022-09-03 17:38:24 +1000

    Added DB and DBConnector

--- src/main/java/dao/DB.java
+++ src/main/java/dao/DB.java
@@ -1,4 +1,14 @@
 package dao;
 
+import java.sql.Connection;
 public class DB {
+
+    // Benz's DB URL --- jdbc:mysql://localhost:3306, db, root, root, com.mysql.cj.jdbc.Driver
+    protected String URL = "jdbc:mysql://localhost:3306";
+    protected String db = "db"; // Please Use This DB Name
+    protected String dbuser = "root"; // Please Use This DB Username here
+    protected String dbpass = "root"; // Please Use This DB Password here
+    protected String driver = "com.mysql.cj.jdbc.Driver";
+    protected Connection conn;
+
 }
