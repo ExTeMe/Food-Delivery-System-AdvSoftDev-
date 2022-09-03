02e0a92502b5ac5c76e2f1c7b62875b5fc4eb12f
BenzChua31 <benzchua31@gmail.com>
2022-09-04 03:23:12 +1000

    Added Part 1 of RMS

--- src/main/java/controller/UpdateCategoryServlet.java
+++ src/main/java/controller/UpdateCategoryServlet.java
@@ -1,14 +1,47 @@
 package controller;
 
+// import controller.Validator; // Validator implementation in the future
  import java.io.IOException;
  import java.sql.Connection;
  import java.sql.SQLException;
+ import java.util.logging.Level;
+ import java.util.logging.Logger;
  import jakarta.servlet.ServletException;
+ import jakarta.servlet.annotation.*;
  import jakarta.servlet.http.HttpServlet;
  import jakarta.servlet.http.HttpServletRequest;
  import jakarta.servlet.http.HttpServletResponse;
  import jakarta.servlet.http.HttpSession;
+ import dao.*;
+ import model.*;
 
- public class UpdateCategoryServlet extends HttpServlet{
+ @WebServlet(name = "UpdateCategoryServlet", value = "update-category")
+ public class UpdateCategoryServlet extends HttpServlet {
+
+      @Override
+      protected void doGet(HttpServletRequest request, HttpServletResponse response)
+              throws ServletException, IOException {
+
+           HttpSession session = request.getSession();
+
+           Restaurant res = (Restaurant) session.getAttribute("restaurant");
+           RCategories categories = (RCategories) session.getAttribute("categories");
+
+           // resID is useless here, since we aren't even interacting with the DBs
+           int resID = Integer.parseInt(request.getParameter("resID"));
+           int cat = Integer.parseInt(request.getParameter("cat"));
+
+           if (res.getRestaurantID() == resID) {
+               for (RCategory category : categories.getCategories()) {
+                   if (category.getRCat_ID() == cat) {
+                       res.setCategory(category);
+                       break;
+                   }
+               }
+           }
+
+           request.getRequestDispatcher("index.jsp").include(request, response);
+
+      }
 
  }
