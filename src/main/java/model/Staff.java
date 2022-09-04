384752a378b8419dda487a2cfdf9496c393e2f34
G1234567 <vcmp1rx1234567@foxmail.com>
2022-09-05 01:04:10 +1000

    Updated Staff Model

--- src/main/java/model/Staff.java
+++ src/main/java/model/Staff.java
@@ -3,17 +3,21 @@ package model;
 import java.time.LocalDate;
 
 public class Staff extends User {
-    private int StaffID;
+    private int staffID;
     private int restaurantID;
     private int privilege;
-    private String positionl;
+    private String position;
 
-    public Staff(int userID, String fname, String lname, String pass, String email, int phoneNo, LocalDate dob, int streetNo, String streetName, int postcode, String state, String suburb, String country, Boolean activated) {
+    public Staff(int userID, String fname, String lname, String pass, String email, int phoneNo, LocalDate dob, int streetNo, String streetName, int postcode, String state, String suburb, String country, Boolean activated, int staffID, int restaurantID, int privilege, String position) {
         super(userID, fname, lname, pass, email, phoneNo, dob, streetNo, streetName, postcode, state, suburb, country, activated);
+        this.staffID = staffID;
+        this.restaurantID = restaurantID;
+        this.privilege = privilege;
+        this.position = position;
     }
 
     public int getStaffID() {
-        return StaffID;
+        return staffID;
     }
 
     public int getRestaurantID() {
@@ -32,11 +36,11 @@ public class Staff extends User {
         this.privilege = privilege;
     }
 
-    public String getPositionl() {
-        return positionl;
+    public String getPosition() {
+        return position;
     }
 
-    public void setPositionl(String positionl) {
-        this.positionl = positionl;
+    public void setPosition(String position) {
+        this.position = position;
     }
 }
