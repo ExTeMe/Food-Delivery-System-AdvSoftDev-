da7574d5d77e7f4552da345560f977431c829e5e
G1234567 <vcmp1rx1234567@foxmail.com>
2022-09-04 23:15:20 +1000

    Updated Staff

--- src/main/java/model/Staff.java
+++ src/main/java/model/Staff.java
@@ -1,101 +1,42 @@
 package model;
 
-public class Staff {
-    private String firstName;
-    private String lastName;
-    private String email;
-    private String phone;
-    private String password;
-    private String dateOfBirth;
+import java.time.LocalDate;
 
-    private String address;
-
+public class Staff extends User {
+    private int StaffID;
+    private int restaurantID;
     private int privilege;
-    private String position;
+    private String positionl;
 
-
-    public Staff(String firstName, String lastName, String email, String phone, String password, String dateOfBirth, String address, int privilege, String position) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phone = phone;
-        this.password = password;
-        this.dateOfBirth = dateOfBirth;
-        this.address = address;
-        this.privilege = privilege;
-        this.position = position;
+    public Staff(int userID, String fname, String lname, String pass, String email, int phoneNo, LocalDate dob, int streetNo, String streetName, int postcode, String state, String suburb, String country, Boolean activated) {
+        super(userID, fname, lname, pass, email, phoneNo, dob, streetNo, streetName, postcode, state, suburb, country, activated);
     }
 
-    public String getFirstName() {
-        return this.firstName;
+    public int getStaffID() {
+        return StaffID;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+    public int getRestaurantID() {
+        return restaurantID;
     }
 
-    public String getLastName() {
-        return this.lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return this.phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getPassword() {
-        return this.password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getDateOfBirth() {
-        return this.dateOfBirth;
-    }
-
-    public void setDateOfBirth(String dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
-
-    public String getAddress() {
-        return this.address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
+    public void setRestaurantID(int restaurantID) {
+        this.restaurantID = restaurantID;
     }
 
     public int getPrivilege() {
-        return this.privilege;
+        return privilege;
     }
 
     public void setPrivilege(int privilege) {
         this.privilege = privilege;
     }
 
-    public String getPosition() {
-        return this.position;
+    public String getPositionl() {
+        return positionl;
     }
 
-    public void setPosition(String position) {
-        this.position = position;
+    public void setPositionl(String positionl) {
+        this.positionl = positionl;
     }
-
 }
