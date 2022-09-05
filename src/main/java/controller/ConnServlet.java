0c0fa256a20f0555b955ed4fc9c0aec468c81e68
William <95123253+will-yau@users.noreply.github.com>
2022-09-06 02:13:36 +1000

    Revert "Updated conServlet"
    This reverts commit 93ebbd4cedb2bed01baa64523b7ba885e15f84d6.

--- src/main/java/controller/ConnServlet.java
+++ src/main/java/controller/ConnServlet.java
@@ -23,7 +23,7 @@ import javax.servlet.http.HttpSession;
 import dao.*;
 import jakarta.servlet.annotation.WebServlet;
 
-@WebServlet(name = "ConnServlet", value = "controller/ConnServlet")
+@WebServlet(name = "controller/ConnServlet", value = "ConnServlet")
 public class ConnServlet extends HttpServlet {
 
     private DBConnector db;
