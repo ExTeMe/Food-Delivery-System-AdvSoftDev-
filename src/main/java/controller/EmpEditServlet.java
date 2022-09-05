0935857326821a9f6a36fbda9f9dca8c5977662b
G1234567 <vcmp1rx1234567@foxmail.com>
2022-09-05 13:46:04 +1000

    Add Edit feature

---
+++ src/main/java/controller/EmpEditServlet.java
@@ -0,0 +1,29 @@
+package controller;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+import model.*;
+
+import java.io.IOException;
+import java.util.*;
+
+@WebServlet("/EmpEdit")
+public class EmpEditServlet extends HttpServlet{
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        List staffs = (LinkedList)this.getServletContext().getAttribute("staffs");
+        int staffId = Integer.parseInt(request.getParameter("staffId"));
+        for(int i = 0; i < staffs.size(); i++)
+        {
+            if(staffId == ((Staff)staffs.get(i)).getStaffID())
+            {
+                ((Staff) staffs.get(i)).setPrivilege(Integer.parseInt(request.getParameter("privilege")));
+                ((Staff) staffs.get(i)).setPosition(request.getParameter("position"));
+            }
+        }
+        request.setAttribute("success", "success");
+        request.getRequestDispatcher("/empManagement.jsp").forward(request, response);
+    }
+}
