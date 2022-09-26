67ca90167c72fc2b025345bfb9b6c5766b813de8
BenzChua31 <benzchua31@gmail.com>
2022-09-26 23:44:19 +1000

    Added SetResCatServlet

--- src/main/java/controller/rms/SetResCatServlet.java
+++ src/main/java/controller/rms/SetResCatServlet.java
@@ -2,7 +2,10 @@ package controller.rms;
 
 // import controller.Validator; // Validator implementation in the future
  import java.io.IOException;
+ import java.util.logging.Level;
+ import java.util.logging.Logger;
 
+ import dao.ResDBManager;
  import jakarta.servlet.ServletException;
  import jakarta.servlet.annotation.*;
  import jakarta.servlet.http.HttpServlet;
@@ -12,32 +15,26 @@ package controller.rms;
  import model.*;
 
  // Update what
- @WebServlet(name = "controller/rms/UpdateCategoryServlet", value = "/update-category")
+ @WebServlet(name = "controller/rms/UpdateCategoryServlet", value = "/update-rescat")
  public class SetResCatServlet extends HttpServlet {
 
       @Override
-      protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      protected void doPost(HttpServletRequest request, HttpServletResponse response)
               throws ServletException, IOException {
 
-//           HttpSession session = request.getSession();
-//
-//           Restaurant res = (Restaurant) session.getAttribute("restaurant");
-//           RCategories categories = (RCategories) session.getAttribute("categories");
-//
-//           // resID is useless here, since we aren't even interacting with the DBs
-//           int resID = Integer.parseInt(request.getParameter("resID"));
-//           int cat = Integer.parseInt(request.getParameter("cat"));
-//
-//           if (res.getRestaurantID() == resID) {
-//               for (RCategory category : categories.getCategories()) {
-//                   if (category.getRCat_ID() == cat) {
-//                       res.setCategory(category);
-//                       break;
-//                   }
-//               }
-//           }
-//
-//           request.getRequestDispatcher("index.jsp").include(request, response);
+            HttpSession session = request.getSession();
+            ResDBManager manager = (ResDBManager) session.getAttribute("ResDBManager");
+
+            String resID = request.getParameter("res");
+            String catID = request.getParameter("cat");
+
+            try {
+                manager.setRestaurantCategory(Integer.parseInt(resID), Integer.parseInt(catID));
+                request.getRequestDispatcher("").include(request, response);
+            } catch (Exception e) {
+                Logger.getLogger(ModifyCategoryServlet.class.getName()).log(Level.SEVERE, null, e);
+                request.getRequestDispatcher("").include(request, response);
+            }
 
       }
 
