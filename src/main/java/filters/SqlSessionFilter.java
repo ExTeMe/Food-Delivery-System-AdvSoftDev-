57e02d426902b568884aa1dfed84909ead73b4f9
ExTeMe <vcmp1rx1234567@foxmail.com>
2022-10-03 22:46:20 +1100

    Add SqlSessionFilter

---
+++ src/main/java/filters/SqlSessionFilter.java
@@ -0,0 +1,26 @@
+package filters;
+
+import jakarta.servlet.*;
+import org.apache.ibatis.session.SqlSession;
+import utils.SqlSessionUtil;
+
+import java.io.IOException;
+
+/**
+ * Filter when the servlet is loaded. Create a SqlSession object and close it at the end
+ */
+public class SqlSessionFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        SqlSession session = SqlSessionUtil.openSqlSession();
+        try{
+             chain.doFilter(request,response);
+             session.commit();
+        }catch (Exception e){
+            session.rollback();
+            e.printStackTrace();
+        }finally {
+            SqlSessionUtil.close(session);
+        }
+    }
+}
